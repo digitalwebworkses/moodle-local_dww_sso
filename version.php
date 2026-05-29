b7028fc0da108ce2f1d52a77a66e0c4a59508f4d
Carlos Manuel Márquez López <digitalwebworks.es@gmail.com>
2026-05-29 13:23:59 +0200

    Bump version to 1.1.0-alpha

--- version.php
+++ version.php
@@ -2,8 +2,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026052701;
+$plugin->version   = 2026052900;
 $plugin->requires  = 2024100700;
 $plugin->component = 'local_dww_sso';
 $plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = '1.0.8-alpha';
+$plugin->release   = '1.1.0-alpha';
